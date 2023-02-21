3e5a983edfba28b0001a0ed039e7638ceb8e0b26
ZayaES <heavenphrince@gmail.com>
2023-02-21 15:14:26 -0800

    2nd test of task4

--- sort.h
+++ sort.h
@@ -43,6 +43,7 @@ void selection_sort(int *array, size_t size);
 int partition(int *array, int first, int last, size_t size);
 void qs(int *array, int first, int last, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 /*.-------------------------------------.
 |					|
